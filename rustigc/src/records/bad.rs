13dd7201239fe886bbe4394629e02f28e9acd598
Jerome Brunet <jbrun3t@gmail.com>
2026-03-16 18:04:26 +0100

    records: bad: handle truncated last line
    Last line may appear truncated. Likely the battery dying.
    
    Signed-off-by: Jerome Brunet <jbrun3t@gmail.com>

--- rustigc/src/records/bad.rs
+++ rustigc/src/records/bad.rs
@@ -2,27 +2,30 @@
 //!
 //! Catch all parser for bad records
 
+use winnow::combinator::{alt, eof, not, repeat};
 use winnow::error::Result as PResult;
-use winnow::token::take_while;
 use winnow::prelude::*;
+use winnow::token::{any, take_while};
 
 use super::Record;
 
 /// Match bad record and store them for analysis in the rawlog
 pub fn bad_record<'a>(input: &mut &'a [u8]) -> PResult<Record<'a>> {
-    // Not using the classic (till_line_ending, line_ending) because it
-    // fails with \r\r\n
-    // ... got to be sure the carriage returned I guess :(
-    (
-        take_while(0.., |c| c != b'\n'),
-        "\n".as_bytes()
-    )
-        .with_taken()
-        .map(|(_, taken): (_, &[u8])| {
-            eprintln!(
-                "Bad Record: {}",
-                std::str::from_utf8(taken).unwrap_or_else(|_| "Non ASCII")
-            );
-            Record::BAD(taken)
-        }).parse_next(input)
+    // Bad record condition:
+    // 1st: handle bad records in the middle of the file, taking care
+    //      to trash multiple carriage returns
+    // 2nd: handle truncated last line
+    alt((
+        (take_while(0.., |c| c != b'\n'), "\n".as_bytes()).void(),
+        (repeat(1.., (not(eof), any))).map(|_: ()| ()),
+    ))
+    .with_taken()
+    .map(|(_, taken): (_, &[u8])| {
+        eprintln!(
+            "Bad Record: {}",
+            std::str::from_utf8(taken).unwrap_or_else(|_| "Non ASCII")
+        );
+        Record::BAD(taken)
+    })
+    .parse_next(input)
 }
