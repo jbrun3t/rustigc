4ed6fa99a774f01d8491e4e8a856c380b2928517
Jerome Brunet <jbrun3t@gmail.com>
2026-08-25 00:08:15 +0200

    python: fix function annotation problem with older pythons
    Signed-off-by: Jerome Brunet <jbrun3t@gmail.com>

--- rustigc-py/python/rustigcpy/log.py
+++ rustigc-py/python/rustigcpy/log.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later WITH Classpath-exception-2.0
 
 """The parsed IGC log and everything derived from it."""
+from __future__ import annotations
+
 from collections.abc import Iterable
 from datetime import UTC, datetime, timedelta
 from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
